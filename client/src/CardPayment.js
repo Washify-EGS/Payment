66e26a8ba68a7c7cdc58c7d6504e32dda32000f4
Tiago Rodrigues <rodriguestiago@ua.pt>
2024-04-09 15:37:38 +0100

    Server with new endpoint that returns client_secret property, web app to be operational

---
+++ client/src/CardPayment.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {useStripe, useElements, PaymentElement} from '@stripe/react-stripe-js';
+
+const CardPayment = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const {error} = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        //return_url: 'https://example.com/order/123/complete',
+      },
+    });
+
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      <button disabled={!stripe}>Submit</button>
+      {/* Show error message to your customers */}
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  )
+};
+
+export default CardPayment;
+
+
+
+/* import {withRouter} from 'react-router-dom'
+import {CardElement} from '@stripe/react-stripe-js';
+
+const CardPayment = () => {
+    return <CardElement/>
+}
+
+export default withRouter(CardPayment); */
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,60 @@
-import React from ' react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
-ReactDOM. render (
-    <React. StrictMode >
-        <App />
-    </ React. StrictMode>,
-    document.getElementById ('root')
-);
+import CheckoutForm from './CheckoutForm';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51OoOS3LthcxqmRhTrRHGxKcER3jdKmy6Y4pweKDwk9yGaIfTUvcs4XA5F4an4wyqcshWlIFoLd5lv26e4j1JiIXE00Mp1MCkKd');
+
+(async () => {
+    const response = await fetch('/secret');
+    const {client_secret: clientSecret} = await response.json();
+    // Render the form using the clientSecret
+  })();
+
+function App() {
+  const options = {
+    // passing the client secret obtained in step 3
+    clientSecret: '{client_secret}',
+    // Fully customizable with appearance API.
+    appearance: {/*...*/},
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+/* import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+
+
+const stripePromise = loadStripe('pk_test_51OoOS3LthcxqmRhTrRHGxKcER3jdKmy6Y4pweKDwk9yGaIfTUvcs4XA5F4an4wyqcshWlIFoLd5lv26e4j1JiIXE00Mp1MCkKd')
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Elements stripe = {stripePromise}>
+        <App/>
+        </Elements>
+    </React.StrictMode>,
+    
+    document.getElementById('root')
+); */
