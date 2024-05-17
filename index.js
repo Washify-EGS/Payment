5995f91e1dbcbfabe321cf247ff25d9a436eda79
João Afonso <ferreiraafonsojoao@ua.pt>
2024-05-17 23:46:45 +0100

    updates

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import fetch from 'node-fetch';
-import stripeModule from 'stripe'; 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import dotenv from "dotenv";
+import express from "express";
+import fetch from "node-fetch";
+import stripeModule from "stripe";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080;
-const environment = process.env.ENVIRONMENT || 'sandbox';
+const environment = process.env.ENVIRONMENT || "sandbox";
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const endpoint_url =
-  environment === 'sandbox'
-    ? 'https://api-m.sandbox.paypal.com'
-    : 'https://api-m.paypal.com';
+  environment === "sandbox"
+    ? "https://api-m.sandbox.paypal.com"
+    : "https://api-m.paypal.com";
 
 const stripeClient = stripeModule(process.env.STRIPE_SECRET_KEY);
 
+var price = 10;
+var currency = "USD";
+const productName = "Product Name";
+
 // Home route
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/templates/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/templates/index.html");
 });
 
 // Create the order - PayPal
-app.post('/create_order', (req, res) => {
+app.post("/create_order", (req, res) => {
   get_access_token()
     .then((access_token) => {
       let order_data_json = {
@@ -38,18 +42,18 @@ app.post('/create_order', (req, res) => {
         purchase_units: [
           {
             amount: {
-              currency_code: 'USD',
-              value: '10.00',
+              currency_code: currency,
+              value: price,
             },
           },
         ],
       };
       const data = JSON.stringify(order_data_json);
 
-      fetch(endpoint_url + '/v2/checkout/orders', {
-        method: 'POST',
+      fetch(endpoint_url + "/v2/checkout/orders", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${access_token}`,
         },
         body: data,
@@ -66,19 +70,19 @@ app.post('/create_order', (req, res) => {
 });
 
 // Complete the order - PayPal
-app.post('/complete_order', (req, res) => {
+app.post("/complete_order", (req, res) => {
   get_access_token()
     .then((access_token) => {
       fetch(
         endpoint_url +
-          '/v2/checkout/orders/' +
+          "/v2/checkout/orders/" +
           req.body.order_id +
-          '/' +
+          "/" +
           req.body.intent,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${access_token}`,
           },
         }
@@ -98,12 +102,12 @@ app.post('/complete_order', (req, res) => {
 // Get the access token - PayPal
 function get_access_token() {
   const auth = `${client_id}:${client_secret}`;
-  const data = 'grant_type=client_credentials';
-  return fetch(endpoint_url + '/v1/oauth2/token', {
-    method: 'POST',
+  const data = "grant_type=client_credentials";
+  return fetch(endpoint_url + "/v1/oauth2/token", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${Buffer.from(auth).toString('base64')}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(auth).toString("base64")}`,
     },
     body: data,
   })
@@ -114,52 +118,104 @@ function get_access_token() {
 }
 
 // Stripe checkout
-app.post('/checkout', async (req, res) => {
-  // const { price, productName } = req.body; // Assuming the price is sent in the request body
-  const price  = 10;
-  const productName = 'Product Name';
-
+app.post("/checkout", async (req, res) => {
   const session = await stripeClient.checkout.sessions.create({
     line_items: [
       {
         price_data: {
-          currency: 'usd',
+          currency: currency,
           product_data: {
-            name: productName 
+            name: productName,
           },
-          unit_amount: price * 100 // Convert price to cents
+          unit_amount: price * 100,
         },
-        quantity: 1 // Assuming only one quantity is purchased
-      }
+        quantity: 1,
+      },
     ],
-    mode: 'payment',
+    mode: "payment",
+    payment_method_types: [
+      "card",
+      "paypal", // Add PayPal
+    ],
+
     shipping_address_collection: {
-      allowed_countries: ['US', 'BR', 'PT', 'ES', 'GB']
+      allowed_countries: ["US", "BR", "PT", "ES", "GB"],
     },
-    success_url: 'http://localhost:8080/complete', // Hardcoded success URL
-    cancel_url: 'http://localhost:8080/cancel' // Hardcoded cancel URL
+    // Redirect back to localhost:8080 after payment completion
+    success_url: "http://localhost:8080/success/?stripe_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "http://localhost:8080/cancel",
+
+    // fetch("http://localhost:8080/complete_order"
+
   });
 
-  console.log(session)
   res.redirect(session.url);
 });
 
-// Stripe checkout complete
-app.get('/complete', async (req, res) => {
-    const result = Promise.all([
-        stripe.checkout.sessions.retrieve(req.query.session_id, { expand: ['payment_intent.payment_method'] }),
-        stripe.checkout.sessions.listLineItems(req.query.session_id)
-    ])
+// Complete the order - Stripe
+app.post('/complete_order_stripe', async (req, res) => {
+  try {
+    const { stripe_id } = req.body;
 
-    console.log(JSON.stringify(await result))
+    // Retrieve the order details from Stripe
+    const order = await stripeClient.checkout.sessions.retrieve(stripe_id, {
+      expand: ['payment_intent.payment_method'],
+    });
 
-    res.send('Your payment was successful')
-})
+    const successfulMessage = `Your payment was successful for order ID ${order.id}`;
+
+    // Send the response back to the client
+    res.json({ message: successfulMessage, order });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred during order completion' });
+  }
+});
+
 
 // Stripe checkout cancel
-app.get('/cancel', (req, res) => {
-    res.redirect('/')
-})
+app.get("/cancel", (req, res) => {
+  res.sendFile(__dirname + "/static/templates/index.html");
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(__dirname + "/static/templates/index2.html");
+});
+
+app.post('/update_price_amount', (req, res) => {
+  try {
+    // Extract the new price and currency from the request body
+    const { amount: newPrice, currency: newCurrency } = req.body;
+
+    // Perform validation on the new price and currency if needed
+    // Update the price and currency variables
+    price = newPrice;
+    currency = newCurrency;
+
+    // Log the updated values (optional)
+    console.log("Updated Price:", price);
+    console.log("Updated Currency:", currency);
+
+    // Send price and currency data back to the client
+    res.json({ price, currency });
+  } catch (error) {
+    // Handle errors
+    console.error("Error updating price and currency:", error);
+    res.status(500).json({ error: 'An error occurred while updating price and currency.' });
+  }
+});
+
+app.get('/get_price_amount', (req, res) => {
+  try {
+    // Send the current price and currency back to the client
+    res.json({ price, currency });
+  } catch (error) {
+    // Handle errors
+    console.error("Error getting price and currency:", error);
+    res.status(500).json({ error: 'An error occurred while getting price and currency.' });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
--- static/js/script.js
+++ static/js/script.js
@@ -1,118 +1,177 @@
-let url_to_head = (url) => {
-    return new Promise(function (resolve, reject) {
-        var script = document.createElement("script");
-        script.src = url;
-        script.onload = function () {
-            resolve();
-        };
-        script.onerror = function () {
-            reject("Error loading script.");
-        };
-        document.head.appendChild(script);
-    });
-};
-
-let handle_click = (event) => {
-    if (event.target.classList.contains("ms-close")) {
-        handle_close(event);
-    }
-};
-
-let handle_close = (event) => {
-    event.target.closest(".ms-alert").remove();
-};
-
 document.addEventListener("DOMContentLoaded", () => {
-    document.addEventListener("click", handle_click);
-
-    // PAYPAL BUTTONS
-    const paypal_sdk_url = "https://www.paypal.com/sdk/js";
-    const client_id = "Abi_EpOMLx3Z1zZj_8es8kSRlqpDYQYZe2dPKQfmSvYyAmCc5YqhIUVSrQyWzRutQzE9T9OmNMCLhlgN";
-    const currency = "USD";
-    const intent = "capture";
+    const urlParams = new URLSearchParams(window.location.search);
+    const stripeId = urlParams.get('stripe_id');
+    let currency = "USD";
     let alerts = document.getElementById("alerts");
+  
+    // If the payment was successful, display the success message
+    if (stripeId) {
+        fetch('http://localhost:8080/complete_order_stripe', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ stripe_id: stripeId })
+        })
+        .then(response => response.json())
+        .then(data => {
+          const alerts = document.getElementById('alerts');
+          if (alerts) {
+            alerts.innerHTML = `
+              <div class='ms-alert ms-action'>
+                Your payment was successful!
+              </div>
+            `;
+          }
+          window.location.href = "/success";
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        })
+        .finally(() => {
+          urlParams.delete('stripe_id');
+          window.history.replaceState({}, '', `${window.location.pathname}?${urlParams.toString()}`);
+        });
+    }
 
-    url_to_head(
-        `${paypal_sdk_url}?client-id=${client_id}&enable-funding=venmo&currency=${currency}&intent=${intent}`
-    )
-    .then(() => {
-        // Handle loading spinner
-        document.getElementById("loading").classList.add("hide");
-        document.getElementById("content").classList.remove("hide");
+    // Fetch the updated price and currency from the server  
+    function updatePriceAndCurrency() {
+        fetch('/get_price_amount')
+          .then(response => response.json())
+          .then(data => {
+            currency = data.currency;
+            var priceCurrencyString = `${data.currency} ${data.price.toFixed(2)}`;
+            if(currency === 'USD') {
+                priceCurrencyString = `$${data.price.toFixed(2)} ${data.currency}`;
+            } else {
+                priceCurrencyString = `€${data.price.toFixed(2)} ${data.currency}`;
+            }
+      
+            // Update the HTML element with the new price and currency
+            document.getElementById('amount').textContent = priceCurrencyString;
 
-        let paypal_buttons = paypal.Buttons({
+            loadPayPalSDK();
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+
+    // Initially update the price and currency and load PayPal SDK
+    updatePriceAndCurrency();
+  
+    // Event handlers
+    let handle_click = (event) => {
+      if (event.target.classList.contains("ms-close")) {
+        handle_close(event);
+      }
+    };
+  
+    let handle_close = (event) => {
+      event.target.closest(".ms-alert").remove();
+    };
+  
+    document.addEventListener("click", handle_click);
+  
+    // Function to render PayPal buttons
+    const renderPayPalButtons = () => {
+        const paymentOptions = document.getElementById("payment_options");
+        if (paymentOptions) {
+          paypal.Buttons({
             onClick: (data) => {
-                // Custom JS here
+              // Custom JS here
             },
             style: {
-                shape: "rect",
-                color: "gold",
-                layout: "vertical",
-                label: "paypal",
+              shape: "rect",
+              color: "gold",
+              layout: "vertical",
+              label: "paypal",
             },
-
+          
             createOrder: function (data, actions) {
-                return fetch("http://localhost:8080/create_order", {
-                    method: "post",
-                    headers: { "Content-Type": "application/json; charset=utf-8" },
-                    body: JSON.stringify({ intent: intent }),
-                })
-                .then((response) => response.json())
-                .then((order) => {
-                    return order.id;
-                });
+              return fetch("http://localhost:8080/create_order", {
+                method: "post",
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: JSON.stringify({ intent: "capture" }),
+              })
+              .then((response) => response.json())
+              .then((order) => {
+                return order.id;
+              });
             },
-
+          
             onApprove: function (data, actions) {
-                let order_id = data.orderID;
-                return fetch("http://localhost:8080/complete_order", {
-                    method: "post",
-                    headers: { "Content-Type": "application/json; charset=utf-8" },
-                    body: JSON.stringify({
-                        intent: intent,
-                        order_id: order_id,
-                    }),
-                })
-                .then((response) => response.json())
-                .then((order_details) => {
-                    console.log(order_details);
-                    let intent_object =
-                        intent === "authorize" ? "authorizations" : "captures";
-                    // Custom Successful Message
-                    alerts.innerHTML =
-                        `<div class='ms-alert ms-action'>Thank you ` +
-                        order_details.payer.name.given_name +
-                        ` ` +
-                        order_details.payer.name.surname +
-                        ` for your payment of ` +
-                        order_details.purchase_units[0].payments[intent_object][0].amount
-                            .value +
-                        ` ` +
-                        order_details.purchase_units[0].payments[intent_object][0].amount
-                            .currency_code +
-                        `!</div>`;
-
-                    // Close out the PayPal buttons that were rendered
-                    paypal_buttons.close();
-                })
-                .catch((error) => {
-                    console.log(error);
-                    alerts.innerHTML = `<div class="ms-alert ms-action2 ms-small"><span class="ms-close"></span><p>An Error Ocurred!</p></div>`;
-                });
+              let order_id = data.orderID;
+              return fetch("http://localhost:8080/complete_order", {
+                method: "post",
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: JSON.stringify({
+                  intent: "capture",
+                  order_id: order_id,
+                }),
+              })
+              .then((response) => response.json())
+              .then((order_details) => {
+                console.log(order_details);
+                let intent_object = "captures"; // Assuming intent is always "capture"
+                // Custom Successful Message
+                if (alerts) {
+                  alerts.innerHTML = `
+                    <div class='ms-alert ms-action'>Thank you ${order_details.payer.name.given_name} ${order_details.payer.name.surname} for your payment of ${order_details.purchase_units[0].payments[intent_object][0].amount.value} ${order_details.purchase_units[0].payments[intent_object][0].amount.currency_code}!</div>`;
+                }
+                window.location.href = "/success";
+                hidePaymentButtons();
+              })
+              .catch((error) => {
+                console.log(error);
+                if (alerts) {
+                  alerts.innerHTML = `<div class="ms-alert ms-action2 ms-small"><span class="ms-close"></span><p>An Error Occurred!</p></div>`;
+                }
+              });
             },
-
+          
             onCancel: function (data) {
+              if (alerts) {
                 alerts.innerHTML = `<div class="ms-alert ms-action2 ms-small"><span class="ms-close"></span><p>Order cancelled!</p></div>`;
+              }
             },
-
+          
             onError: function (err) {
-                console.log(err);
+              console.log(err);
             },
-        });
+          }).render("#payment_options")
+            .then(() => {
+              // Handle loading spinner
+              document.getElementById("loading").classList.add("hide");
+              document.getElementById("content").classList.remove("hide");
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+    };
 
-        paypal_buttons.render("#payment_options");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    // Function to load PayPal SDK dynamically with updated currency
+    const loadPayPalSDK = () => {
+        const paypal_sdk_url = `https://www.paypal.com/sdk/js?client-id=${client_id}&enable-funding=venmo&currency=${currency}&intent=${intent}`;
+        const script = document.createElement("script");
+        script.src = paypal_sdk_url;
+        script.onload = () => {
+            renderPayPalButtons();
+        };
+        script.onerror = () => {
+            console.error("Error loading PayPal SDK.");
+        };
+        document.head.appendChild(script);
+    };
+
+    // PayPal Buttons configurations
+    const client_id = "Abi_EpOMLx3Z1zZj_8es8kSRlqpDYQYZe2dPKQfmSvYyAmCc5YqhIUVSrQyWzRutQzE9T9OmNMCLhlgN";
+    const intent = "capture";
+    
+    // Function to handle hiding payment buttons
+    const hidePaymentButtons = () => {
+      const paymentOptions = document.getElementById("payment_options");
+      if (paymentOptions) {
+        paymentOptions.innerHTML = ""; // Clear the content of the payment options container
+      }
+    };
 });
